be6d575103aa404919ec38f97036cbe00142ecda
Jack Kavanagh <jackkav@gmail.com>
2019-11-15 16:04:17 +0100

    add wait

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 extern crate nickel;
-
+use std::process::Command;
 use nickel::status::StatusCode;
 use nickel::{HttpRouter, MiddlewareResult, Nickel, QueryString, Request, Response};
 //todo, wait, headers
 
 fn handle<'mw>(_req: &mut Request, mut res: Response<'mw>) -> MiddlewareResult<'mw> {
     let query = _req.query();
-    let statuscode: u16 = query.get("statuscode").unwrap().parse().unwrap();
-    println!("Returning {:?} Code", statuscode);
-    res.set(StatusCode::Unregistered(statuscode).class().default_code());
+    if query.get("statuscode").is_some() {
+        let statuscode: u16 = query.get("statuscode").unwrap().parse().unwrap();
+        println!("Returning {:?} code", statuscode);
+        res.set(StatusCode::Unregistered(statuscode).class().default_code());
+    }
+    if query.get("wait").is_some() {
+        let wait = query.get("wait").unwrap();
+        println!("Waiting {:?}", wait);
+        let mut child = Command::new("sleep").arg(wait).spawn().unwrap();
+        let _result = child.wait().unwrap();
+    }
     res.send("hello world")
 }
 
