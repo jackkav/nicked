60a2f153cc41a610f56b39fc2f1e34c9d0262b96
Jack Kavanagh <jackkav@gmail.com>
2019-11-15 15:38:06 +0100

    status codes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-#[macro_use]
 extern crate nickel;
 
 use nickel::status::StatusCode;
-use nickel::{HttpRouter, Nickel};
+use nickel::{HttpRouter, MiddlewareResult, Nickel, QueryString, Request, Response};
+//todo, wait, headers
+
+fn handle<'mw>(_req: &mut Request, mut res: Response<'mw>) -> MiddlewareResult<'mw> {
+    let query = _req.query();
+    let statuscode: u16 = query.get("statuscode").unwrap().parse().unwrap();
+    println!("Returning {:?} Code", statuscode);
+    res.set(StatusCode::Unregistered(statuscode).class().default_code());
+    res.send("hello world")
+}
 
 fn main() {
     let mut server = Nickel::new();
-    server.get(
-        "/:statuscode",
-        middleware! { |request|
-            println!("statuscode: {:?}",request.param("statuscode").unwrap());
-            let my_string = request.param("statuscode").unwrap();
-            let my_int: u16 = my_string.parse().unwrap();
-            StatusCode::Unregistered(my_int).class().default_code()
-        },
-    );
-    // server.get("**", middleware!("Hello World"));
+    server
+        .get("**", handle)
+        .post("**", handle)
+        .put("**", handle);
     server.listen("127.0.0.1:6767").unwrap();
 }
