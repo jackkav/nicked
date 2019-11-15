c2304915158028e43983b1314d50b9ccb71f16b6
Jack Kavanagh <jackkav@gmail.com>
2019-11-15 14:07:40 +0100

    can respond with custom status code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate nickel;
+
+use nickel::status::StatusCode;
+use nickel::{HttpRouter, Nickel};
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+    server.get(
+        "/:statuscode",
+        middleware! { |request|
+            println!("statuscode: {:?}",request.param("statuscode").unwrap());
+            let my_string = request.param("statuscode").unwrap();
+            let my_int: u16 = my_string.parse().unwrap();
+            StatusCode::Unregistered(my_int).class().default_code()
+        },
+    );
+    // server.get("**", middleware!("Hello World"));
+    server.listen("127.0.0.1:6767").unwrap();
 }
